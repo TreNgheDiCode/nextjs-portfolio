a60e9cfefbd843fbf7d82fff3c33b3366512c8d6
TreNgheDiCode <gabayan170@gmail.com>
2024-07-10 13:08:37 +0700

    Bento Grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import { HomeIcon } from "lucide-react";
@@ -16,6 +17,7 @@ export default function Home() {
       <div className="w-full max-w-7xl">
         <FloatingNav navItems={navItems} />
         <Hero />
+        <Grid />
       </div>
     </main>
   );
